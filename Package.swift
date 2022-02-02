1e12df9c1d03bf1979dc37ed8cf369b08d35af71
dipeshpatel7733-hub <85125016+dipeshpatel7733-hub@users.noreply.github.com>
2022-02-02 17:44:15 +0530

    removed extra required platforms.

--- Package.swift
+++ Package.swift
@@ -6,9 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ModuleApp",
     platforms: [
-      .iOS(.v13),
-      .iOS(.v14),
-      .iOS(.v15)
+      .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
