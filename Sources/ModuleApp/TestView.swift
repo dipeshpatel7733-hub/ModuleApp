c39c7e984194747fce71bb20b5a08cf467ff85de
dipeshpatel7733-hub <85125016+dipeshpatel7733-hub@users.noreply.github.com>
2022-02-02 17:07:59 +0530

    Added testview in sources.

---
+++ Sources/ModuleApp/TestView.swift
@@ -0,0 +1,23 @@
+//
+//  TestView.swift
+//  Main App
+//
+//  Created by dipesh patel on 02/02/22.
+//
+
+import SwiftUI
+
+public struct TestView: View {
+
+  public init() {}
+
+  public var body: some View {
+    Text("Hey there...")
+  }
+}
+
+struct TestView_Previews: PreviewProvider {
+  static var previews: some View {
+      TestView()
+  }
+}
