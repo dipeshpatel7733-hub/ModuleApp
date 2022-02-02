0bf8d32951c424f601da4f070223664eb865564d
dipeshpatel7733-hub <85125016+dipeshpatel7733-hub@users.noreply.github.com>
2022-02-02 17:25:57 +0530

    Added path in package file.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ModuleApp",
-            dependencies: []),
+            dependencies: [],
+            path: "Module App"),
         .testTarget(
             name: "ModuleAppTests",
             dependencies: ["ModuleApp"],
