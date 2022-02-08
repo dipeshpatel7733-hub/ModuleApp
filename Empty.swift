351cd926a808cfb0ae8533295f31219cb7ec2112
dipeshpatel7733-hub <85125016+dipeshpatel7733-hub@users.noreply.github.com>
2022-02-08 10:04:05 +0530

    Added r.swift dependancy.

---
+++ Empty.swift
@@ -0,0 +1,8 @@
+//
+//  Empty.swift
+//  Module App
+//
+//  Created by dipesh patel on 08/02/22.
+//
+
+import Foundation
--- Package.swift
+++ Package.swift
@@ -8,13 +8,12 @@ let package = Package(
     platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "ModuleApp",
-            targets: ["ModuleApp"]),
+        .library(name: "ModuleApp", targets: ["ModuleApp"]),
+        .executable(name: "rswift", targets: ["ModuleApp"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/mac-cain13/R.swift", from: "5.4.0"),
+         .package(url: "https://github.com/mac-cain13/R.swift.Library", from: "5.4.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
