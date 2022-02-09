d8638e9053d8b2999c650fec78e1ee15ce281ebd
dipeshpatel7733-hub <85125016+dipeshpatel7733-hub@users.noreply.github.com>
2022-02-09 10:18:33 +0530

    changed R.swift package URL.

--- BuildTools/Package.swift
+++ BuildTools/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
   name: "BuildTools",
   dependencies: [
-    .package(url: "https://github.com/mac-cain13/R.swift", from: "5.4.0"),
+    .package(url: "https://github.com/mac-cain13/R.swift.Library", from: "5.4.0"),
   ],
   targets: [.target(name: "BuildTools", path: "")]
 )
